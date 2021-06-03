9edb863b83cf1b90be0b78758efbceb5a9608087
Magic Len <len@magiclen.org>
2021-06-03 15:10:59 +0800

    update .eslintrc

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -5,12 +5,12 @@ import * as QRCode from "..";
 
 describe("Encode QR Code", function () {
     it("should encode a V1 QR code.", function () {
-        let result = QRCode.encode("https://magiclen.org".toUpperCase());
+        const result = QRCode.encode("https://magiclen.org".toUpperCase());
         expect(result.length).to.equal(21);
     });
 
     it("should encode a V2 QR code.", function () {
-        let result = QRCode.encode("https://magiclen.org");
+        const result = QRCode.encode("https://magiclen.org");
         expect(result.length).to.equal(25);
     });
 });
